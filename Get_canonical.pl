b3d4beb0c3c10a35375eac17c07bdf74469775e3
rochevin <vincent.rocher@etu.univ-rouen.fr>
2015-04-07 17:44:40 +0200

    Ecriture des exons et des séquences fasta correspondante en utilisant l'API perl d'Ensembl via release 75

--- Get_canonical.pl
+++ Get_canonical.pl
@@ -1,70 +1,98 @@
 use Bio::EnsEMBL::Registry;
+use Bio::SeqIO;
+
+
+$ENV{MYSQL_UNIX_PORT} = "/Applications/MAMP/tmp/mysql/mysql.sock";
+
+unless ( open(file_list, $ARGV[0]) ) {
+    print STDERR "Impossible de trouver $file_list ...\n\n";
+    exit;
+}
+
+my %transcript_list;
+
+foreach my$line (<file_list>) {
+	if ( $line =~ /^\s*$/ ){
+    	next;
+    }
+    else {
+    	my @line_content = split(" ", $line);
+    	$transcript_list{$line_content[0]}{"Gene_ID"}=$line_content[1];
+    	$transcript_list{$line_content[0]}{"Trans_ID"}=$line_content[2];
+    }
+}
+close file_list;
+
+# Fichier qui va servir à annoter les exons de chaques transcrits canoniques, pour pouvoir comparer avec nos transcrits, si ils ont autant d'introns
+unless ( open(exon_file_output, ">".$ARGV[1]) ) {
+    print STDERR "Impossible de trouver $file_accession_output ...\n\n";
+    exit;
+}
+print exon_file_output "RefSeq\tEnsembl Transcript ID\tEnsembl Gene ID\tEnsembl Exon ID\tExon number\tChr\tStrand\tCoordinates\n";
+
+my $fasta_file_out = ">".$ARGV[2];
+my $out = Bio::SeqIO->new(-file => $fasta_file_out, -format => "Fasta");
+
+unless ( open(cds_file_output, ">".$ARGV[3]) ) {
+    print STDERR "Impossible de trouver $file_accession_output ...\n\n";
+    exit;
+}
 
 
 my $r = "Bio::EnsEMBL::Registry";
 
 $r->load_registry_from_db(-host => "localhost", -user => "root", -pass => "root", -port => "8889", -verbose => "0");
 
+
 my $gene_adaptor=$r->get_adaptor("Human" ,"core", "Gene");
 
-my $gene = $gene_adaptor->fetch_by_stable_id('ENSG00000000003');
+foreach my$id (keys %transcript_list) {
+	my $gene = $gene_adaptor->fetch_by_stable_id($transcript_list{$id}{"Gene_ID"});
 
-my $canonical_transcript=$gene->canonical_transcript();
+	my $canonical_transcript=$gene->canonical_transcript();
+	my $transcript = $canonical_transcript->stable_id();
 
-print $canonical_transcript->stable_id();
-# 	my $r ="Bio::EnsEMBL::Registry";
+	print $gene->stable_id()." => ".$canonical_transcript->stable_id()."\n";
+	my @exons = @{ $canonical_transcript->get_all_Exons };
+	
+	my $exon_number = 1;
+	while (my$exon = shift @exons) {
+		my $stable_id  = $exon->stable_id();
+    	my $seq_region = $exon->seq_region_name();
+    	my $start      = $exon->start();
+    	my $end        = $exon->end();
+    	my $strand     = $exon->strand();
+    	my $content = sprintf ("%s\t%s\t%s\t%s\t%d\tchr%s\t%+d\t%s_%s\n",$id,$transcript,$transcript_list{$id}{"Gene_ID"},$stable_id,$exon_number,$seq_region,$strand,$start,$end);
+    	print exon_file_output $content;
+		$exon_number++;
+	}
 
- 
-# 	# Pour chaque id de la liste gene ...
-# 	foreach my $main_id (@gene_id) {
-# 		# On définit une nouvelle connexion à la base de donnée ensembl
-# 		$r->load_registry_from_db(-host => "ensembldb.ensembl.org", -user => "anonymous", -verbose => "0");
-# 		# On se connecte à la bonne base de donnée
-# 		my $orga = $dataset{$main_id.'_Taxonomy_Name'};
-# 		my $gene_adaptor=$r->get_adaptor($orga ,"core", "Gene");
+	my @translateable_exons = @{ $canonical_transcript->get_all_translateable_Exons };
+	while (my$exon = shift @translateable_exons) {
 
-# 		# On récupère l'information grâce au gene symbol
-# 		my $gene = $gene_adaptor->fetch_by_display_label($dataset{$main_id.'_Gene_Symbol'});
-
-# 		$dataset{$main_id.'_ENS_geneID'}=$gene->stable_id(); #On enregistre l'id du gene 
-# 		# On enregistre ses coordonnées
-# 		$dataset{$main_id.'_ENS_browserLOC'}=$gene->seq_region_name().":".$gene->seq_region_start()."-".$gene->seq_region_end();
-# 		# On obtient la liste de tous les transcrits de ce gene
-# 		my@trans=@{$gene->get_all_Transcripts()};
-# 		my@liste_prot=();
-# 		# On parcours ces transcrits...
-# 		foreach $trans (@trans) {
-# 			if (defined $trans->translation()) { #Si il existe une proteine pour ce transcrit
-# 				$prot=$trans->translation();
-# 				# Alors on enregistre dans dataset un tableau associatif avec comme clé l'id du transcrit et sa valeur l'id de la protéine
-# 				$dataset{$main_id.'_ENS_transID'}{$trans->stable_id()}=$prot->stable_id;
-# 			}
-# 			else {
-# 				# Sinon on enregistre juste l'id du transcrit comme clé et "none" comme valeur
-# 				$dataset{$main_id.'_ENS_transID'}{$trans->stable_id()}="none";
-# 			}
-# 		}
-# 		# On récupère le transcrit canonique définit par ensembl -> nous servira à récupérer l'id uniprot "principal"
-# 		my $canonical_transcript=$gene->canonical_transcript();
-# 		$canonical_prot=$canonical_transcript->translation();
-# 		# De la même façon on enregistre la clé id transcrit canonique et sa valeur la protéine
-# 		$dataset{$main_id.'_ENS_canonical_transID'}{$canonical_transcript->stable_id()}=$canonical_prot->stable_id();
+		my $stable_id  = $exon->stable_id();
+    	my $seq_region = $exon->seq_region_name();
+    	my $start      = $exon->start();
+    	my $end        = $exon->end();
+    	my $strand     = $exon->strand();
+    	my $content = sprintf ("%s\t%s\t%s\t%s\t%d\tchr%s\t%+d\t%s_%s\n",$id,$transcript,$transcript_list{$id}{"Gene_ID"},$stable_id,$exon_number,$seq_region,$strand,$start,$end);
+    	print cds_file_output $content;
 
 
-# 		# À utiliser uniquement si on veut récupérer les termes GO via la base de donnée EnsEMBL
+		$exonseqobj = $exon->seq();
+		$exonseqobj->description($id.":".$transcript_list{$id}{"Gene_ID"});
+		$out->write_seq($exonseqobj);
+	}
+	my @introns = @{ $canonical_transcript->get_all_Introns };
+	while (my$intron = shift @introns) {
+		$tag = $intron->prev_Exon->stable_id()."_".$intron->next_Exon->stable_id();
+		$intronseqobj = Bio::Seq->new( -display_id => $tag,
+                             -seq => $intron->seq(),
+                             -description => $id.":".$transcript_list{$id}{"Gene_ID"});
+		$out->write_seq($intronseqobj);
+	}
 
-# 		# my @dblinks = @{$canonical_transcript->get_all_DBLinks("GO")};
-# 		# my %verif_GO_term;
-# 		# while (my $dblinks = shift @dblinks) {
-# 			# my $GO_id = $dblinks->display_id;
-# 			# unless (exists $verif_GO_term{$GO_id}) {
-# 	      		# $verif_GO_term{$GO_id}=1;
-# 	      		# push @{$dataset{$main_id.'_ENS_GOTERM'}},$GO_id;
-# 			# }
-# 		# }
-# 	print "EnsEMBL => ".$main_id." OK !\n";
-# 	}
-# 	# On renvoit le tableau associatif avec les nouvelles données dedans
-# 	return %dataset;
-# }
-# return 1;
+}
+$out->close();
+close exon_file_output;
+close cds_file_output;
