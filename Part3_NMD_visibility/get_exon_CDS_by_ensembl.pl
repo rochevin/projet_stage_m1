ff30ce4f67084f4c121e51582b3bf85cb527085d
rochevin <vincent.rocher@etu.univ-rouen.fr>
2015-04-13 17:53:28 +0200

    script terminé et fonctionnel, reste les commentaires

--- Part3_NMD_visibility/get_exon_CDS_by_ensembl.pl
+++ Part3_NMD_visibility/get_exon_CDS_by_ensembl.pl
@@ -17,6 +17,9 @@ foreach my$line (<file_list>) {
 	if ( $line =~ /^\s*$/ ){
     	next;
     }
+    elsif ( $line =~ /^Ensembl/ ) {
+    	next;
+    }
     else {
     	my @line_content = split("\t", $line);
     	$transcript_list{$line_content[0]}=$line_content[1];
@@ -24,11 +27,42 @@ foreach my$line (<file_list>) {
 }
 close file_list;
 
+my $fasta_file_out = ">".$ARGV[1];
+my $out = Bio::SeqIO->new(-file => $fasta_file_out, -format => "Fasta");
+unless ( open(file_out, ">".$ARGV[2]) ) {
+    print STDERR "Impossible de trouver $ARGV[0] ...\n\n";
+    exit;
+}
 
 my $r = "Bio::EnsEMBL::Registry";
 
 $r->load_registry_from_db(-host => "localhost", -user => "root", -pass => "root", -port => "8889", -verbose => "0");
 
-$transcript_adaptor = r->get_adaptor( 'Human', 'Core','Transcript' );
+my $transcript_adaptor = $r->get_adaptor( 'Human', 'Core','Transcript' );
 
-$transcript = $transcript_adaptor->fetch_by_stable_id('ENST00000201961');
+foreach my$transcript_id (keys %transcript_list) {
+
+	#On récupère tout d'abbord nos identifiants de chaque exon, pour pouvoir associer les exons CDS qui sortiront à nos données
+	# my %coord_for_each_exon = reverse %{$exon_by_transcript{$transcript}} if exists($exon_by_transcript{$transcript});
+	
+
+	my $transcript = $transcript_adaptor->fetch_by_stable_id($transcript_id);
+
+	my $seq_region = $transcript->seq_region_name();
+	my $strand;
+	if ($transcript->strand() < 1) {$strand = "-";}
+	else {$strand = "+";}
+
+	my @exons = @{ $transcript->get_all_translateable_Exons() };
+
+	while (my $exon = shift @exons) {
+		my $coords_for_one_exon = $exon->start().":".$exon->end();
+		my $seq = $exon->seq();
+		$out->write_seq($seq);
+		print file_out $exon->stable_id()."\t".$transcript_id."\t".$transcript_list{$transcript_id}."\t"."chr".$seq_region.$strand.$coords_for_one_exon."\n";
+	}
+
+}
+
+$out->close();
+close file_out;
