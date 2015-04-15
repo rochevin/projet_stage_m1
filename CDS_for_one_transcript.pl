56e0e81b54675b44afc1cbf9bc0c1db1eab6a3b3
rochevin <vincent.rocher@etu.univ-rouen.fr>
2015-04-15 17:15:29 +0200

    Ajout d'un petit scrip perl qui affiche pour un transcrit : son CDS, la sequence de ces exons et la sequences des exons du CDS

---
+++ CDS_for_one_transcript.pl
@@ -0,0 +1,30 @@
+use Bio::EnsEMBL::Registry;
+use Bio::SeqIO;
+use Data::Dumper;
+
+$ENV{MYSQL_UNIX_PORT} = "/Applications/MAMP/tmp/mysql/mysql.sock";
+
+my $r = "Bio::EnsEMBL::Registry";
+
+$r->load_registry_from_db(-host => "localhost", -user => "root", -pass => "root", -port => "8889", -verbose => "0");
+
+my $transcript_adaptor = $r->get_adaptor( 'Human', 'Core','Transcript' );
+
+my $transcript = $transcript_adaptor->fetch_by_stable_id($ARGV[0]);
+
+print "CDS de $ARGV[0] :\n".$transcript->translateable_seq(), "\n";
+
+print "Exons du CDS : \n";
+my @exons = @{ $transcript->get_all_translateable_Exons() };
+while (my $exon = shift @exons) {
+	print $exon->stable_id()."\n";
+	print $exon->seq->seq."\n";
+}
+
+
+print "Tous les exons : \n";
+my @exons = @{ $transcript->get_all_Exons() };
+while (my $exon = shift @exons) {
+	print $exon->stable_id()."\n";
+	print $exon->seq->seq."\n";
+}
