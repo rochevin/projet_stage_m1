a0bfd879f197f8e1659460f1627e21bde3b65156
rochevin <vincent.rocher@etu.univ-rouen.fr>
2015-04-16 17:29:44 +0200

    Ajout pour voir la position du start et du stop dans le transcrit

--- CDS_for_one_transcript.pl
+++ CDS_for_one_transcript.pl
@@ -12,6 +12,8 @@ my $transcript_adaptor = $r->get_adaptor( 'Human', 'Core','Transcript' );
 
 my $transcript = $transcript_adaptor->fetch_by_stable_id($ARGV[0]);
 
+print "$ARGV[0] :".$transcript->seq_region_start."-".$transcript->seq_region_end."\n";
+
 print "CDS de $ARGV[0] :\n".$transcript->translateable_seq(), "\n";
 
 print "Exons du CDS : \n";
@@ -26,5 +28,19 @@ print "Tous les exons : \n";
 my @exons = @{ $transcript->get_all_Exons() };
 while (my $exon = shift @exons) {
 	print $exon->stable_id()."\n";
+	print $exon->start()."-".$exon->end()."\n";
 	print $exon->seq->seq."\n";
 }
+
+
+print "sequence de $ARGV[0] :\n".$transcript->seq->seq, "\n";
+
+print $transcript->cdna_coding_start."\n";
+print $transcript->cdna_coding_end."\n";
+
+
+
+# @features = @{$feature->get_all_alt_locations()};
+# foreach $f (@features) {
+# 	print $f->slice->seq_region_name,' ',$f->start, $f->end,"\n";
+# }
